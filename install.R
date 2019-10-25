f0fa0e6038653689a8ac23cae124a910360a472c
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2019-10-25 09:11:12 +0200

    add config files for binder

---
+++ install.R
@@ -0,0 +1,8 @@
+install.packages("httr")
+install.packages("devtools")
+install.packages("BiocManager")
+install.packages("rphylopic")
+install.packages("plyr")
+install.packages("dplyr")
+install.packages("ggimage")
+
